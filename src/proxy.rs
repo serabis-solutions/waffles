0182976e5caac346d8e35ef419c8ab6832befccc
Graeme Lawton <graeme.lawton@cartridgesave.co.uk>
2017-09-22 21:24:55 +0100

    Added rustfmt. Run on entire project using cargo fmt (must ran using nightly).

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,14 +1,14 @@
 use futures;
-use futures::{Future};
+use futures::Future;
 use hyper;
-use hyper::{Client, Body, StatusCode};
-use hyper::client::{HttpConnector};
-use hyper::server::{Service, Request, Response};
+use hyper::{Body, Client, StatusCode};
+use hyper::client::HttpConnector;
+use hyper::server::{Request, Response, Service};
 use config::Config;
 
 pub struct Proxy {
-    pub config :Config,
-    pub client :Client<HttpConnector, Body>,
+    pub config: Config,
+    pub client: Client<HttpConnector, Body>,
 }
 
 impl Service for Proxy {
@@ -23,39 +23,44 @@ impl Service for Proxy {
 }
 
 
-fn proxy_request(config: &Config, client: &Client<HttpConnector, Body>, req: Request) -> Box<Future<Item=Response, Error=hyper::Error>> {
+fn proxy_request(
+    config: &Config,
+    client: &Client<HttpConnector, Body>,
+    req: Request,
+) -> Box<Future<Item = Response, Error = hyper::Error>> {
     let proxy_url = format!(
         "http://{}:{}",
-        config.get::<String>("proxy.address").expect("unable to load proxy address from config"),
-        config.get::<String>("proxy.port").expect("unable to load proxy address from config")
+        config
+            .get::<String>("proxy.address")
+            .expect("unable to load proxy address from config"),
+        config
+            .get::<String>("proxy.port")
+            .expect("unable to load proxy address from config")
     );
 
     debug!("Proxy url: {}", proxy_url);
-    let mut proxied_request :Request = hyper::client::Request::new(
-        req.method().clone(), 
-        proxy_url.parse::<hyper::Uri>().expect("Unable to parse URI")
+    let mut proxied_request: Request = hyper::client::Request::new(
+        req.method().clone(),
+        proxy_url
+            .parse::<hyper::Uri>()
+            .expect("Unable to parse URI"),
     );
 
     *proxied_request.headers_mut() = req.headers().clone();
 
     let req = client.request(proxied_request);
 
-    Box::new(req.then(|res| {
-        if let Ok(res) = res {
-            debug!("Got a response!");
-            futures::future::ok(
-                Response::new()
-                    .with_status(res.status().clone())
-                    .with_headers(res.headers().clone())
-                    .with_body(res.body())
-            )
-        } else {
-            debug!("Didn't got a response! :(");
-            debug!("Error? {:?}", res);
-            futures::future::ok(
-                Response::new()
-                    .with_status(StatusCode::ServiceUnavailable)
-            )
-        }
+    Box::new(req.then(|res| if let Ok(res) = res {
+        debug!("Got a response!");
+        futures::future::ok(
+            Response::new()
+                .with_status(res.status().clone())
+                .with_headers(res.headers().clone())
+                .with_body(res.body()),
+        )
+    } else {
+        debug!("Didn't got a response! :(");
+        debug!("Error? {:?}", res);
+        futures::future::ok(Response::new().with_status(StatusCode::ServiceUnavailable))
     }))
 }
