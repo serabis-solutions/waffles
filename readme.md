3c3ce1d7d2f5b90b0b38b496b4052a2699593231
Graeme Lawton <graeme.lawton@cartridgesave.co.uk>
2017-09-28 17:20:15 +0100

    Added SSL Support

--- readme.md
+++ readme.md
@@ -12,3 +12,13 @@ rustup run nightly cargo run --features clippy
 rustup run nightly cargo install rustfmt-nightly
 rustup run nightly cargo fmt
 ```
+
+## Create TLS Cert For Testing
+Waffles uses p12 format TLS Certificates. You can create one as follows:
+
+```
+mkdir cert
+openssl req -newkey rsa:2048 -x509 -keyout cert/test_key.pem -out cert/test.csr -days 3650
+openssl pkcs12 -export -in cert/test.csr -inkey cert/test_key.pem -out cert/identity.p12 -name "testkey"
+```
+Currently passwordless certs aren't supported.
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ extern crate futures;
 extern crate hyper;
 #[macro_use]
 extern crate log;
+extern crate native_tls;
 extern crate pretty_env_logger;
 extern crate tokio_core;
+extern crate tokio_tls;
 
 mod errors;
 mod proxy;
@@ -21,6 +23,11 @@ use tokio_core::net::TcpListener;
 use hyper::server::Http;
 use errors::*;
 use hyper::Client;
+use std::io::{Read};
+use std::fs::File;
+use native_tls::{Pkcs12, TlsAcceptor};
+use config::Config;
+use tokio_tls::TlsAcceptorExt;
 
 fn run() -> Result<()> {
     info!("loading config from Waffles.toml");
@@ -46,8 +53,79 @@ fn run() -> Result<()> {
     let mut core = Core::new().expect("Failed to get handle from core");
     let handle = core.handle();
 
-    let client = Client::new(&handle);
-    let listener = TcpListener::bind(&(addr.parse().expect("Invalid listen address?")), &handle)
+
+    if config
+        .get::<bool>("listen.secure")
+        .expect("listen::secure is missing from config?")
+    {
+        run_https(&config, &addr, &handle, &mut core);
+    } else {
+        run_http(&config, &addr, &handle, &mut core);
+    }
+
+    Ok(())
+}
+
+fn run_https(
+    config: &Config,
+    addr: &str,
+    handle: &tokio_core::reactor::Handle,
+    core: &mut Core,
+) {
+    let client = Client::new(handle);
+    let http = Http::new();
+    let sock = TcpListener::bind(&(addr.parse().expect("Invalid listen address?")), handle)
+        .expect("Failed to setup listening server");
+
+    info!(
+        "Listening on https://{}",
+        sock.local_addr()
+            .expect("Unable to parse listening address")
+    );
+
+    let mut cert_file = File::open(&config
+        .get::<String>("listen.certificate")
+        .expect("No listen::certificate file specified"))
+        .expect("Failed to open certificate file");
+    let mut cert = Vec::new();
+    cert_file
+        .read_to_end(&mut cert)
+        .expect("Failed to read in cert file");
+
+    let pkcs12 = Pkcs12::from_der(
+        &cert,
+        &config
+            .get::<String>("listen.password")
+            .expect("No certificate password found in config"),
+    ).expect("Failed to parse pkcs12 certificates, is the password correct?");
+    let acceptor = TlsAcceptor::builder(pkcs12)
+        .expect("Failed to build https builder")
+        .build()
+        .expect("Failed to build https listener");
+
+    let server = sock.incoming().for_each(|(sock, remote_addr)| {
+        let service = proxy::Proxy {
+            config: config.clone(),
+            client: client.clone(),
+        };
+        acceptor
+            .accept_async(sock)
+            .join(Ok(remote_addr))
+            .and_then(|(sock, remote_addr)| {
+                http.bind_connection(handle, sock, remote_addr, service);
+                Ok(())
+            })
+            .or_else(|e| {
+                info!("Error accepting TLS connection: {}", e);
+                Ok(())
+            })
+    });
+    core.run(server).expect("Unable to start https server");
+}
+
+fn run_http(config: &Config, addr: &str, handle: &tokio_core::reactor::Handle, core: &mut Core) {
+    let client = Client::new(handle);
+    let listener = TcpListener::bind(&(addr.parse().expect("Invalid listen address?")), handle)
         .expect("Failed to setup listening server");
 
     info!("Listening on http://{}", listener.local_addr().unwrap());
@@ -60,13 +138,14 @@ fn run() -> Result<()> {
             client: client.clone(),
         };
         futures::future::ok(remote_addr).and_then(|remote_addr| {
-            http.bind_connection(&handle, sock, remote_addr, service);
+            http.bind_connection(handle, sock, remote_addr, service);
             Ok(())
         })
     });
+
     core.run(server).expect("Unable to start server");
-    Ok(())
 }
+
 
 fn main() {
     pretty_env_logger::init().unwrap();
